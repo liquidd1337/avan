1ab296995d7e314b96bf874f3ccfc1f013de892f
liquidd1337 <mikle-1910@mail.ru>
2024-11-14 20:44:58 +0700

    fix path config

--- src/avan/read_config.rs
+++ src/avan/read_config.rs
@@ -1,9 +1,9 @@
 use std::fs::File;
 use std::io::{self, BufRead};
-use std::path::Path;
+
 
 // Вспомогательная функция для чтения файла и поиска нужной строки
-fn read_config_line(keyword: &str) -> Option<String> {
+fn read_config(keyword: &str) -> Option<String> {
     if let Ok(file) = File::open("config.txt") {
         for line in io::BufReader::new(file).lines() {
             if let Ok(line) = line {
@@ -17,23 +17,23 @@ fn read_config_line(keyword: &str) -> Option<String> {
 }
 
 pub fn get_api_key() -> String {
-    read_config_line("API Key Steam:").unwrap_or_default()
+    read_config("API Key Steam: \"").unwrap_or_default()
 }
 
 pub fn get_trade_url() -> String {
-    read_config_line("Ссылка на обмен:").unwrap_or_default()
+    read_config("Ссылка на обмен: \"").unwrap_or_default()
 }
 
 pub fn get_percent_value() -> f64 {
-    read_config_line("Желаемый % пополнения:")
+    read_config("Желаемый % пополнения: \"")
         .and_then(|val| val.parse().ok())
         .unwrap_or(0.0)
 }
 
 pub fn get_login() -> String {
-    read_config_line("login:").unwrap_or_default()
+    read_config("login: \"").unwrap_or_default()
 }
 
 pub fn get_password() -> String {
-    read_config_line("pass:").unwrap_or_default()
+    read_config("pass: \"").unwrap_or_default()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 pub mod avan;
 pub mod client_http2;
 
-#[tokio::main]
-async fn main() {
-    println!("{}", avan::read_config::get_trade_url());
+
+fn main() {
+    println!("{}", avan::read_config::get_percent_value());
 }
