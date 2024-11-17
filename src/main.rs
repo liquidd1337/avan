30f63cbcfd2d5faec34e234fce2740a11ad258db
liquidd1337 <mikle-1910@mail.ru>
2024-11-17 17:57:57 +0700

    fix main

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ pub mod client_http2;
 #[tokio::main]
 async fn main() {
 
-    println!("{}", avan::read_config::get_smp_value());
-    //avan::parse_avan::parse_avan().await;
+    //println!("{}", avan::read_config::get_smp_value());
+    avan::parse_avan::parse_avan().await;
 }
